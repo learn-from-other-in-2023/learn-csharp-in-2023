177c968376bf5d54faffb504b017ae338adc7b74
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-26 08:54:36 +0530

    Swamy/25nov2022 work (#1)
    * Initial Folder Structure
    
    * Update learn-csharp-dp-dsa.sln
    
    * Folder Structure
    
    * Header Footer

---
+++ Source/CSharp/HeaderFooter.Lib/Extensions/HeaderFooterExtensions.cs
@@ -0,0 +1,20 @@
+﻿using HeaderFooter.Lib.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HeaderFooter.Lib.Extensions
+{
+
+    public static class HeaderFooterExtensions
+    {
+
+        public static IServiceCollection ConfigureNamesArrayDemoServices(this IServiceCollection services)
+        {
+            _ = services.AddTransient<IFooter, Footer>();
+            _ = services.AddTransient<IHeader, Header>();
+
+            return services;
+        }
+
+    }
+
+}
